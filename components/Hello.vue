663d28e624d2001ba919956d6326b94d6d7247d8
linyows <linyows@gmail.com>
2019-02-05 00:45:02 +0900

    update Hello

--- components/Hello.vue
+++ components/Hello.vue
@@ -1,8 +1,55 @@
 <template>
   <div class="hello">
-     <p>毎年東京で開催されているGo Conferenceが、ついに今年は福岡開催があります。会場は天神あたりで７月を予定しております。</p>
+    <p>私たちソフトウェア開発者にとってなくてはならないプログラミング言語Goは、ビジネスの世界でも開発のメイン言語にGoを選択するケースが増えてきました。
+    JetBrainsの2018年の調査では、「<a href="https://www.jetbrains.com/research/devecosystem-2018/">Goは最も有望なプログラミング言語</a>」と評価されました。</p>
+    <p>このように、実績と知名度を伸ばしているGoのConferenceが、今年はついに福岡開催があります。
+    日本のシリコンバレーと言われている福岡で、Goを通じて様々な技術の話をし、学び合い、楽しい時間を過ごし、開発者同士の交流を深めましょう！</p>
+
+    <dl class="meta">
+      <dt>日時</dt>
+      <dd>2019年7月2週目を予定</dd>
+      <dt>会場</dt>
+      <dd>福岡市天神付近を予定</dd>
+    </dl>
   </div>
 </template>
 
 <style scoped>
+.hello p {
+  color: #666;
+  text-align: left;
+  padding-bottom: 1em;
+  text-indent: 1em;
+  font-weight: normal;
+}
+.hello p a {
+  color: #888;
+  border-bottom: 1px solid #888;
+}
+.meta {
+    width: 14em;
+    margin: 3em auto 0;
+    font-size: 1.4em;
+    border-bottom: 1px solid #888;
+}
+.meta dt {
+    font-weight: bold;
+    width: 3em;
+    float: left;
+    border-top: 1px solid #888;
+    padding-top: .3em;
+    padding-bottom: .2em;
+    text-align: center;
+}
+.meta dd {
+    padding-left: 3em;
+    border-top: 1px solid #888;
+    padding-top: .3em;
+    padding-bottom: .2em;
+}
+.meta dd:after {
+  content: '';
+  display: block;
+  clear: both;
+}
 </style>
