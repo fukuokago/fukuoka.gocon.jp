4fb764a43cebf7db4e745741056f78a0b8b909c3
linyows <linyows@gmail.com>
2019-02-05 00:44:24 +0900

    update CFP

--- components/CallForPapers.vue
+++ components/CallForPapers.vue
@@ -1,9 +1,48 @@
 <template>
   <div class="call-for-papers">
     <h2>Call for Papers</h2>
-    <p>𝝣GO 𝝣GO 𝝣GO</p>
+    <p>もうすぐGoCon2019 Summer in Fukuokaの登壇者を募集を開始します。Twitterなどでアナウンスしますのでしばらくお待ちください。</p>
+
+    <h3>Talk types</h3>
+    <ul>
+      <li>Keynote style presentations; 20 minute time limit</li>
+      <li>Tutorial style presentations; 40 minute time limit</li>
+    </ul>
+
+    <h3>The selection criteria</h3>
+    <ol>
+      <li>Relevance. The talk is relevant to the Go community. GoCon is not a general software conference, our audience wants to hear about topics that relate to the Go programming language.</li>
+      <li>Clarity. You’ve clearly explained what you are going to talk about.</li>
+      <li>Correctness. You’ve demonstrated knowledge of your topic. You don’t have to be an expert, but you are expected to be speaking from experience.</li>
+      <li>Achievability. You’ve thought about how to present your material in the time available.</li>
+      <li>Impact. The goal of the talk. What new idea, technique, tool, or information will the audience leave your presentation with?</li>
+    </ol>
   </div>
 </template>
 
 <style scoped>
+.call-for-papers p {
+  text-align: left;
+  font-weight: normal;
+  padding-bottom: 1em;
+  color: #888;
+}
+.call-for-papers h3 {
+  font-size: 2em;
+  padding-bottom: .5em;
+}
+.call-for-papers ul {
+  font-size: 1.3em;
+  list-style-type: circle;
+  padding-left: 1em;
+  padding-bottom: 1em;
+}
+.call-for-papers ol {
+  font-size: 1.3em;
+  padding-left: 1em;
+}
+.call-for-papers li {
+  line-height: 1.7;
+  color: #888;
+}
 </style>
