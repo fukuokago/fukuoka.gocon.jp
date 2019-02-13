65a8a69e1672c3334a15d4fff7e2921cc511c540
linyows <linyows@gmail.com>
2019-02-14 07:28:56 +0900

    add Fukuoka component

---
+++ components/Fukuoka.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="fukuoka">
+    <h2>About Fukuoka</h2>
+    <p><a href="http://www.city.fukuoka.lg.jp/shisei/profile">福岡市</a>は、九州最大の都市であり都市部と空港までの規模がコンパクトに収まっていて、気候は温暖であることから、
+    アジアで最も住みやすい都市のひとつと言われています。
+    福岡市は、<a ref="http://hakatanomiryoku.com/mamecategory/food">食文化</a>も発達していて、豚骨ラーメン、もつ鍋を筆頭に水炊き、やきとり、餃子、明太子などの福岡名物の食べ物が数多く存在します。
+    最近では、福岡市の進める「<a href="https://efc.isit.or.jp/">エンジニアフレンドリーシティ</a>」によって、都市全体でIT分野の活性化が顕著です。</p>
+
+    <ul>
+      <li><a href="https://www.trivago.jp/?aDateRange%5Barr%5D=2019-02-22&aDateRange%5Bdep%5D=2019-02-23&aPriceRange%5Bfrom%5D=0&aPriceRange%5Bto%5D=0&iPathId=90333&aGeoCode%5Blat%5D=33.590355&aGeoCode%5Blng%5D=130.401718&iGeoDistanceItem=0&aCategoryRange=0%2C1%2C2%2C3%2C4%2C5&aOverallLiking=1%2C2%2C3%2C4%2C5&sOrderBy=relevance%20desc&bTopDealsOnly=false&iRoomType=7&cpt=9033303&iIncludeAll=0&iViewType=0&bIsSeoPage=false&bIsSitemap=false&">福岡市のホテルを探す</a></li>
+      <li><a href="https://yokanavi.com/">福岡市のガイド</a></li>
+    </ul>
+  </section>
+</template>
+
+<style scoped>
+.news {
+  margin-bottom: 2em;
+}
+.news p.date {
+  margin-top: -.5em;
+  margin-bottom: .2em;
+  font-weight: bold;
+  color: #ccc;
+  font-family: 'Arvo', serif;
+}
+.news p {
+  margin-bottom: .5em;
+}
+.news ul {
+  font-size: .8em;
+  list-style-type: circle;
+  padding-left: 1em;
+  padding-bottom: 1em;
+}
+.news li {
+  line-height: 1.8;
+  color: #888;
+}
+</style>
