ad4dd46e77de2a5fb0cd9d19cf1e2425172f6b50
linyows <linyows@gmail.com>
2019-02-15 12:16:22 +0900

    change to spa from ssr

--- components/Team.vue
+++ components/Team.vue
@@ -3,32 +3,32 @@
     <h2>Team</h2>
     <div class="member">
         <img src="https://avatars3.githubusercontent.com/u/1661325?s=120&v=4" />
-        <h3 class="name">@keita_kawamoto</h3>
+        <h3 class="name"><a href ="https://dribbble.com/keita_kawamoto">@keita_kawamoto</a></h3>
         <p class="role">Designer</p>
     </div>
     <div class="member">
         <img src="https://avatars3.githubusercontent.com/u/72049?s=120&v=4" />
-        <h3 class="name">@linyows</h3>
+        <h3 class="name"><a href ="https://twitter.com/linyows">@linyows</a></h3>
         <p class="role">Organizer, Designer</p>
     </div>
     <div class="member">
-        <img src="https://avatars3.githubusercontent.com/u/1845486?s=120&v=4" />
-        <h3 class="name">@monochromegane</h3>
+        <img src="https://d19ta9rijs3cxg.cloudfront.net/wp-content/uploads/sites/2/2017/05/13692462_1013755355398990_5321412467621857881_n-e1494916040772.jpg" />
+        <h3 class="name"><a href ="https://twitter.com/monochromegane">@monochromegane</a></h3>
         <p class="role">Organizer</p>
     </div>
     <div class="member">
         <img src="https://avatars3.githubusercontent.com/u/796820?s=120&v=4" />
-        <h3 class="name">@tenntenn</h3>
+        <h3 class="name"><a href ="https://twitter.com/tenntenn">@tenntenn</a></h3>
         <p class="role">Organizer</p>
     </div>
     <div class="member">
         <img src="https://avatars3.githubusercontent.com/u/145104?s=120&v=4" />
-        <h3 class="name">@ymotongpoo</h3>
+        <h3 class="name"><a href ="https://twitter.com/ymotongpoo">@ymotongpoo</a></h3>
         <p class="role">Organizer</p>
     </div>
     <div class="member">
         <img src="https://avatars3.githubusercontent.com/u/2609671?s=120&v=4" />
-        <h3 class="name">@yuji-otani</h3>
+        <h3 class="name"><a href ="https://www.facebook.com/yuji.otani.16">@yuji-otani</a></h3>
         <p class="role">Organizer</p>
     </div>
   </div>
