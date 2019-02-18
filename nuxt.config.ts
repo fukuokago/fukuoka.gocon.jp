32431d826eda79cadc5f6c70bf5c882cd01bf36c
linyows <linyows@gmail.com>
2019-02-18 15:48:26 +0900

    no scale in viewport

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ const config = {
     titleTemplate: `%s - ${name}`,
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no' },
       { hid: 'description', name: 'description', content: desc },
       { name: 'keywords', content: '' },
       { property: 'og:title', content: name },
