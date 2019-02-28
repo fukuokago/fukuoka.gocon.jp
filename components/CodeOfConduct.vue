3e964e04220c95fd37266f4ae1385ee224365e2c
monochromegane <dev.kuro.obi@gmail.com>
2019-02-28 18:33:00 +0900

    Add CoC page.

--- components/CodeOfConduct.vue
+++ components/CodeOfConduct.vue
@@ -6,8 +6,8 @@ en:
   summary_2nd: 'Be thoughtful in how you communicate.'
   summary_3rd: 'Don’t be destructive or inflammatory.'
 ja:
-  description: '私たちは参加者と登壇者が以下のGoコミュニティ行動規範に従うことを求めています'
-  note: '注意：これらはサマリーです。すべての内容を読むにはこちらからお願いします。'
+  description: '私たちは参加者と登壇者が以下のGoコミュニティ行動規範に従うことを求めています。'
+  note: '注意：これらはサマリーです。すべての内容を読むにはこちらからお願いします'
   summary_1st: 'みんなに敬意を払い優しく接しましょう'
   summary_2nd: '伝達には配慮しましょう'
   summary_3rd: '物事を壊そうとしたり煽ったりしてはいけません'
@@ -16,13 +16,13 @@ ja:
 <template>
   <div class="code-of-conduct">
     <h2>Code of Conduct</h2>
-    <p>{{ $t('description') }}
-    {{ $t('note') }} <a href="https://golang.org/conduct">https://golang.org/conduct</a></p>
+    <p>{{ $t('description') }}</p>
     <ul>
       <li>{{ $t('summary_1st') }}</li>
       <li>{{ $t('summary_2nd') }}</li>
       <li>{{ $t('summary_3rd') }}</li>
     </ul>
+    <p>{{ $t('note') }} 👉 <a href="https://golang.org/conduct">https://golang.org/conduct</a></p>
   </div>
 </template>
 
---
+++ pages/code-of-conduct.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container">
+    <CodeOfConduct/>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import CodeOfConduct from '~/components/CodeOfConduct.vue'
+
+@Component({
+  head() {
+    return {
+      title: 'Code of Conduct',
+    }
+  },
+  components: {
+    CodeOfConduct
+  }
+})
+export default class PageCodeOfConduct extends Vue {}
+</script>
+
+<style scoped>
+</style>
