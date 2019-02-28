3f45ec5a31b0937dd725eae20235168693f2ae12
seike460 <seike460@gmail.com>
2019-02-28 23:39:39 +0900

    :+1: /registration i18n

--- components/Registration.vue
+++ components/Registration.vue
@@ -1,7 +1,14 @@
+<i18n>
+en:
+  description: 'Participation will be accepted from Connpass.'
+ja:
+  description: '参加はConnpassから受け付ける予定です'
+</i18n>
+
 <template>
   <div class="registration">
     <h2>Registration</h2>
-    <p>参加はConnpassから受け付ける予定です。</p>
+    <p>{{ $t('description') }}</p>
 
     <h3>Registration types</h3>
     <ul>
