07319aba606c0d64438f39342c4959b1edb6f099
linyows <linyows@gmail.com>
2019-03-08 19:33:26 +0900

    fire after page rendering

--- plugins/pattern-wall.ts
+++ plugins/pattern-wall.ts
@@ -1,24 +1,43 @@
 import Vue, { VueConstructor } from 'vue'
 
-interface IVuePatternWallOptions {
-}
+class VuePatternWall {
+  public static install(vueConstructor: VueConstructor<Vue>): never | void {
+    const pattern = new VuePatternWall()
 
-const VuePatternWall = {
-  defaultOption: {
-  },
-  install(vueConstructor: VueConstructor<Vue>, options?: IVuePatternWallOptions): void {
-    console.log(options)
-    const emit: Vue['$emit'] = vueConstructor.prototype.$emit
-    console.log(emit)
+    window.addEventListener('resize', () => {
+      console.log('resized')
+    }, { passive: true })
+
+    Vue.directive('patternwall', {
+      inserted (el, binding) {
+        pattern.add(el, binding)
+      },
+      unbind(el, binding){
+        pattern.remove(el, binding)
+      }
+    })
+
+    vueConstructor.mixin({
+      mounted() {
+        vueConstructor.prototype.$nextTick(() => {
+          if ('constructor' in this) {
+            const n = this.constructor.name
+            if (n.match(/^Page/) !== null) {
+              console.log(n, document.body.clientHeight)
+            }
+          }
+        })
+      }
+    })
+  }
+
+  public remove(el: any, binding: any) {
+    console.log(el, binding)
+  }
+
+  public add(el: any, binding: any) {
+    console.log(el, binding)
   }
 }
 
 Vue.use(VuePatternWall)
-
-/*
-declare module 'vue/types/vue' {
-  interface Vue {
-    $patternwall(event: string, ...args: any[]): Vue;
-  }
-}
-*/
