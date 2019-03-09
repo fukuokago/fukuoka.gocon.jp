ee845117da027cc57512b32cfe98dd00f4545948
linyows <linyows@gmail.com>
2019-03-09 12:42:56 +0900

    constructor name no get if production

--- plugins/pattern-wall.ts
+++ plugins/pattern-wall.ts
@@ -7,12 +7,16 @@ class VuePatternWall {
         vueConstructor.prototype.$nextTick(() => {
           if ('constructor' in this) {
             const n = this.constructor.name
-            if (n.match(/^Page/) !== null) {
+            const v = this as Vue
+            v.$store.commit('pages/updatePage', n.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase())
+            /* if (n.match(/^Page/) !== null) {
               if ('$store' in this) {
-                const v = this as Vue
-                v.$store.commit('pages/updatePage', n.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase())
+                const el = document.getElementById('pattern-wall')
+                if (el !== null) {
+                  el.style['height'] = `${document.body.clientHeight}px`
+                }
               }
-            }
+            }*/
           }
         })
       }
