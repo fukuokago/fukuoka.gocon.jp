f09022170ade6e258513f6e0d35187a11d8f65db
linyows <linyows@gmail.com>
2019-03-12 00:25:14 +0900

    update brand page

--- components/Brand.vue
+++ components/Brand.vue
@@ -14,13 +14,64 @@ ja:
     <h2>Brand</h2>
     <p>{{ $t('desc') }}</p>
 
-    <h3>Wallpaper</h3>
-    <ul class="colored">
-      <li><a href="/wallpaper/gopher-noodle.png">Gopher Noodle</a> (for Phone)</li>
-      <li><a href="/wallpaper/gophers-pattern.png">Gophers Pattern</a> (for Phone)</li>
-    </ul>
+    <div class="logo">
+      <h3>Logo</h3>
+      <p><a class="button" href="/logo/go-conference-19-summer-in-fukuoka.tar.gz">Download Logo Assets</a></p>
+    </div>
+
+    <div class="is-clearfix">
+      <h3>Wallpaper</h3>
+
+      <div class="wallpaper">
+        <picture class="eyecatch">
+          <source type="image/webp" srcset="~static/img/gopher-noodle.webp">
+          <img src="~assets/img/gopher-noodle.png" >
+        </picture>
+        <p>Gopher Noodle</p>
+        <ul>
+          <li><a class="button" href="/wallpaper/gopher-noodle-phone.png">For Phone</a></li>
+          <li><a class="button" href="/wallpaper/gopher-noodle-laptop.png">For Laptop</a></li>
+        </ul>
+      </div>
+
+      <div class="wallpaper">
+        <picture class="eyecatch">
+          <source type="image/webp" srcset="~static/img/gophers-pattern.webp">
+          <img src="~assets/img/gophers-pattern.png" >
+        </picture>
+        <p>Gophers Pattern</p>
+        <ul>
+          <li><a class="button" href="/wallpaper/gophers-pattern-phone.png">For Phone</a></li>
+          <li><a class="button" href="/wallpaper/gophers-pattern-laptop.png">For Laptop</a></li>
+        </ul>
+      </div>
+    </div>
   </section>
 </template>
 
 <style scoped>
+.logo p {
+  padding-top: 2em;
+}
+
+.wallpaper {
+  width: 400px;
+  float: left;
+  padding: 2em 1em 0 0;
+}
+.eyecatch {
+  width: 200px;
+  float: left;
+  padding: 0 2em 0 0;
+}
+.eyecatch img {
+  width: 194px;
+  border: 3px solid #fff;
+}
+.wallpaper ul {
+  list-style-type: none;
+}
+.wallpaper li {
+  margin-bottom: .5em;
+}
 </style>
