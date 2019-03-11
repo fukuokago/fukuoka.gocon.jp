ec0bdc44fd8e53d2cfa66fd90a283e5a3a8e6b4b
linyows <linyows@gmail.com>
2019-03-11 11:42:06 +0900

    add error.vue

---
+++ components/TheNotFound.vue
@@ -0,0 +1,22 @@
+<i18n>
+en:
+  desc: The link you clicked on may be broken or no longer exist.
+  back: Back to the home page
+ja:
+  desc: クリックしたリンクが壊れているか、もう存在しない可能性があります。
+  back: ホームにもどる
+</i18n>
+
+<template>
+  <section class="notfound container">
+    <h2>This page doesn't exist</h2>
+    <p>{{ $t('desc') }}</p>
+    <p><nuxt-link :to="localePath('/')">Back to the home page</nuxt-link></p>
+  </section>
+</template>
+
+<style scoped>
+.notfound {
+  min-height: 10em;
+}
+</style>
