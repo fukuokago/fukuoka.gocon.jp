735def4ff7e3e3c28b41966370731b209f9b3cf2
linyows <linyows@gmail.com>
2019-05-10 10:36:33 +0900

    disable cfp button in top

--- components/About.vue
+++ components/About.vue
@@ -1,6 +1,7 @@
 <i18n>
 en:
   callforpapers: Call for Papers
+  cfpclosed: Closed
   registration: Registration
   enjoy: "In Fukuoka, let's talk about various technologies through the Go programming language, learn from each other, have a good time, interact with developers! You will surely feel that Fukuoka is an attractive city for engineers by attending the conference."
   background: '"{go}" is been held from early in comparison with other one in the world and a significant event for Go community. Every year several hundreds of gophers attend the conference and interact through various sessions of Go.'
@@ -8,6 +9,7 @@ en:
   theme: 'Catchphrase is {words}'
 ja:
   callforpapers: 登壇募集に応募する
+  cfpclosed: 終了
   registration: 参加登録をする
   enjoy: 'エンジニアにとって魅力ある土地、福岡で、今注目を集めるプログラミング言語Goを通じて様々な技術の話をし、学び合い、楽しい時間を過ごし、開発者同士の交流を深めましょう！'
   background: '「{go}」は、世界的に見ても初期から行われており、Goコミュニティにとって意義のあるイベントです。カンファレンスでは毎年数百名のGopherが参加しGoにまつわる多様なセッションを通した交流が行われています。'
@@ -31,8 +33,9 @@ ja:
     </i18n>
 
     <div class="attention">
-      <p class="cfp"><nuxt-link class="button" :to="localePath('cfp')"><span>📝</span>{{ $t('callforpapers') }}</nuxt-link></p>
-      <p class="registration"><nuxt-link class="button" :to="localePath('registration')"><span>🎪</span>{{ $t('registration') }}</nuxt-link></p>
+      <p class="cfp"><nuxt-link class="button" :to="localePath('cfp')"><span>📝</span>
+          <s>{{ $t('callforpapers') }}</s>{{ $t('cfpclosed') }}</nuxt-link></p>
+      <p class="registration"><nuxt-link class="button is-focused" :to="localePath('registration')"><span>🎪</span>{{ $t('registration') }}</nuxt-link></p>
     </div>
   </div>
 </template>
@@ -44,6 +47,9 @@ ja:
 }
 .attention p {
   display: inline-block;
+}
+.attention s {
+    padding-right: .5em;
 }
 .attention .cfp {
   padding-right: 1em;
