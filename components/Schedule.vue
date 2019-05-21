f2a9fc977945c3ed2302801f0fcc3f59278fb02b
linyows <linyows@gmail.com>
2019-05-21 12:38:13 +0900

    rebuild timetable design

--- components/Schedule.vue
+++ components/Schedule.vue
@@ -17,148 +17,183 @@ ja:
 
     <p>{{ $t('desc') }}</p>
 
-    <table><tbody>
-      <tr>
-        <th class="time" colspan="1" rowspan="1"><p>Time</p></th>
-        <th class="roomA" colspan="1" rowspan="1"><p>Room1</p></th>
-        <th class="roomB" colspan="1" rowspan="1"><p>Room2</p></th></tr>
-      <tr class="common">
-        <td class="time" colspan="1" rowspan="1"><p>10:00-10:10</p></td>
-        <td colspan="2" rowspan="1"><p>Opening talk 🙋</p></td></tr>
-      <tr>
-        <td class="time" colspan="1" rowspan="1"><p>10:10-10:50</p></td>
-        <td class="roomA" colspan="1" rowspan="1"><p>Long talk</p></td>
-        <td class="roomB" colspan="1" rowspan="1"><p>Long talk</p></td></tr>
-      <tr>
-        <td class="time" colspan="1" rowspan="1"><p>10:50-11:10</p></td>
-        <td class="roomA" colspan="1" rowspan="1"><p>Sponsor talk 📢</p></td>
-        <td class="roomB" colspan="1" rowspan="1"><p>Sponsor talk 📢</p></td></tr>
-      <tr>
-        <td class="time" colspan="1" rowspan="1"><p>11:10-11:30</p></td>
-        <td class="roomA" colspan="1" rowspan="1"><p>Sponsor talk 📢</p></td>
-        <td class="roomB" colspan="1" rowspan="1"><p>Sponsor talk 📢</p></td></tr>
-      <tr>
-        <td class="time" colspan="1" rowspan="1"><p>11:30-11:50</p></td>
-        <td class="roomA" colspan="1" rowspan="1"><p>Short talk</p></td>
-        <td class="roomB" colspan="1" rowspan="1"><p>Short talk</p></td></tr>
-      <tr class="common">
-        <td class="time" colspan="1" rowspan="1"><p>11:50-13:20</p></td>
-        <td class="break lunch" colspan="2" rowspan="1"><p>Lunch break 🍜</p></td></tr>
-      <tr>
-        <td class="time" colspan="1" rowspan="1"><p>13:20-13:40</p></td>
-        <td class="roomA" colspan="1" rowspan="1"><p>Sponsor talk 📢</p></td>
-        <td class="roomB" colspan="1" rowspan="1"><p>Sponsor talk 📢</p></td></tr>
-      <tr>
-        <td class="time" colspan="1" rowspan="1"><p>13:40-14:00</p></td>
-        <td class="roomA" colspan="1" rowspan="1"><p>Short talk</p></td>
-        <td class="roomB" colspan="1" rowspan="1"><p>Short talk</p></td></tr>
-      <tr>
-        <td class="time" colspan="1" rowspan="1"><p>14:00-14:20</p></td>
-        <td class="roomA" colspan="1" rowspan="1"><p>Short talk</p></td>
-        <td class="roomB" colspan="1" rowspan="1"><p>Short talk</p></td></tr>
-      <tr>
-        <td class="time" colspan="1" rowspan="1"><p>14:20-15:00</p></td>
-        <td class="roomA" colspan="1" rowspan="1"><p>Long talk</p></td>
-        <td class="roomB" colspan="1" rowspan="1"><p>Long talk</p></td></tr>
-      <tr class="common">
-        <td class="time" colspan="1" rowspan="1"><p>15:00-15:10</p></td>
-        <td class="break" colspan="2" rowspan="1"><p>Break ☕️</p></td></tr>
-      <tr>
-        <td class="time" colspan="1" rowspan="1"><p>15:10-15:30</p></td>
-        <td class="roomA" colspan="1" rowspan="1"><p>Short talk</p></td>
-        <td class="roomB" colspan="1" rowspan="1"><p>Short talk</p></td></tr>
-      <tr>
-        <td class="time" colspan="1" rowspan="1"><p>15:30-15:50</p></td>
-        <td class="roomA" colspan="1" rowspan="1"><p>Short talk</p></td>
-        <td class="roomB" colspan="1" rowspan="1"><p>Short talk</p></td></tr>
-      <tr>
-        <td class="time" colspan="1" rowspan="1"><p>15:50-16:30</p></td>
-        <td class="roomA" colspan="1" rowspan="1"><p>Long talk</p></td>
-        <td class="roomB" colspan="1" rowspan="1"><p>Long talk</p></td></tr>
-      <tr class="common">
-        <td class="time" colspan="1" rowspan="1"><p>16:30-16:50</p></td>
-        <td class="break" colspan="2" rowspan="1"><p>Break ☕️</p></td></tr>
-      <tr>
-        <td class="time" colspan="1" rowspan="1"><p>16:50-17:10</p></td>
-        <td class="roomA" colspan="1" rowspan="1"><p>Sponsor talk 📢</p></td>
-        <td class="roomB" colspan="1" rowspan="1"><p>Sponsor talk 📢</p></td></tr>
-      <tr>
-        <td class="time" colspan="1" rowspan="1"><p>17:10-17:30</p></td>
-        <td class="roomA" colspan="1" rowspan="1"><p>Short talk</p></td>
-        <td class="roomB" colspan="1" rowspan="1"><p>Short talk</p></td></tr>
-      <tr>
-        <td class="time" colspan="1" rowspan="1"><p>17:30-17:50</p></td>
-        <td class="roomA" colspan="1" rowspan="1"><p>Short talk</p></td>
-        <td class="roomB" colspan="1" rowspan="1"><p>Short talk</p></td></tr>
-      <tr class="common">
-        <td class="time" colspan="1" rowspan="1"><p>17:50-18:50</p></td>
-        <td colspan="2" rowspan="1"><p>Networking 🍻</p></td></tr>
-      <tr class="common">
-        <td class="time" colspan="1" rowspan="1"><p>18:50-19:00</p></td>
-        <td colspan="2" rowspan="1"><p>Close 🍽</p></td></tr>
-    </tbody></table>
+    <div class="row" v-for="(sessions, time) in timetable">
+        <p class="timediv">{{ time }}</p>
+        <div class="is-clearfix">
+          <div class="session" v-bind:class="{single: sessions.length === 1}" v-for="(v, i) in sessions">
+            <p class="session--time">{{ format24To12(v.start) }} - {{ format24To12(v.end) }}
+              <span class="session--time--duration">({{ duration(v.start, v.end) }})</span>
+            </p>
+            <p class="session--title">{{ v.title }}</p>
+            <div class="session--speaker is-clearfix">
+              <img class="session--speaker--avatar" :src="v.avatar" v-if="v.avatar !== undefined" />
+              <p class="session--speaker--name">{{ v.name }}</p>
+              <p class="session--speaker--org" v-if="v.organization !== undefined">{{ v.organization }}</p>
+            </div>
+          </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
+const t = require('~/data/timetable.json')
+const speakers = require('~/data/speakers.json')
+
 @Component
-export default class Schedule extends Vue {}
+export default class Schedule extends Vue {
+  public timetable
+
+  public format24To12(t: string): string {
+    const colonRemoved = t.replace(/:/, '')
+    const tt = parseInt(colonRemoved)
+    const f12h = `${tt - (tt > 1200 ? 1200 : 0)}`.replace(/^(.*)(\d{2})$/, '$1:$2')
+    return `${f12h} ${tt > 1200 ? 'PM' : 'AM'}`
+  }
+
+  public duration(start: string, stop: string): string {
+    const s = start.split(':')
+    const ss = stop.split(':')
+    const us = Math.round((new Date(1970, 0, 1, parseInt(s[0]), parseInt(s[1]))).getTime() / 1000 / 60)
+    const uss = Math.round((new Date(1970, 0, 1, parseInt(ss[0]), parseInt(ss[1]))).getTime() / 1000 / 60)
+    const duMin = uss - us
+    if (duMin >= 60) {
+      const h = Math.floor(duMin / 60)
+      const m = duMin - (h * 60)
+      if (m === 0) {
+        return `${h} hours`
+      }
+      return `${h} hours, ${m} min`
+    }
+    return `${duMin} min`
+  }
+
+  public findSpeaker(id: string) {
+    for (const v of speakers) {
+      if (v.id === id) {
+        return v
+      }
+    }
+    return {}
+  }
+
+  public createDiv() {
+    let divs = {}
+    for (const v of t) {
+      if (v.start === '') {
+        continue
+      }
+
+      const sp = this.findSpeaker(v.id)
+      const speaker = { ...sp, ...v }
+      const time = this.format24To12(v.start)
+      if (divs[time] === undefined) {
+        divs[time] = []
+      }
+      divs[time].push(speaker)
+    }
+    return divs
+  }
+
+  public data() {
+    return {
+      timetable: this.createDiv()
+    }
+  }
+}
 </script>
 
 <style scopd>
-.schedule table {
+.row {
+  position: relative;
+  padding: 0 0 2em 7em;
+}
+.timediv {
+  position: absolute;
+  top: 10px;
+  left: 0;
+  background-color: #000;
+  color: #fff;
+  display: inline-block;
+  font-size: 11px;
+  padding: .5em 1.5em;
+  font-weight: bold;
+  font-family: 'Arvo', serif;
+  border-radius: 5px;
+}
+.session {
+  position: relative;
+  border: 1px solid #CCC;
+  border-radius: 6px;
+  width: 48%;
+  float: left;
+  padding: 1em 1em 0 1.5em;
+}
+.session:first-child {
+  margin-right: 4%;
+}
+.single.session {
   width: 100%;
-  font-size: .8em;
-  opacity: 0.6;
+  float: none;
 }
-.schedule table p {
-  text-align: center;
-  color: #000;
-  font-weight: bold;
-}
-.schedule td,
-.schedule th {
-  text-align: center;
-  background-color: #fff;
-  border-top: 1px solid #e5e5e5;
-}
-.schedule td p,
-.schedule th p {
-  font-weight: normal;
-  color: #111;
-  padding-top: .4em;
-  padding-bottom: .3em;
-}
-.schedule th p {
-  font-weight: bold;
-}
-.schedule .common p {
-  color: #999;
-}
-.schedule .time {
-  background-color: #ddd;
-}
-.schedule .time p {
-  color: #000;
-}
-.schedule .roomB {
-  background-color: #f5f5f5;
-}
-.schedule .common td {
+.session--time:before {
+  content: '●';
+  position: absolute;
+  top: 9px;
+  left: -17px;
+  font-size: 2em;
+  border: 1px solid #CCC;
+  border-radius: 5px;
+  padding: 0 10px;
   background-color: #e5e5e5;
+  color: #e5e5e5;
+  font-size: 17px;
 }
-.schedule .common td p {
-  color: #666;
+.session--time {
+  font-size: 11px;
+  font-weight: bold;
+  font-family: 'Arvo', serif;
 }
-@media (min-width: 401px) and (max-width: 500px) {
-  .schedule table {
-    font-size: .6em;
+.session--time--duration {
+  font-weight: normal;
+  padding-left: .5em;
+}
+.session--title {
+  font-weight: bold;
+}
+.session--speaker {
+  padding-bottom: 1em;
+  font-family: 'Arvo', serif;
+}
+.session--speaker img {
+  border-radius: 50%;
+  width: 81px;
+  height: 81px;
+  overflow: hidden;
+  background-color: #fff;
+  float: left;
+  padding: .2em;
+  margin-right: 1em;
+}
+.session--speaker--name {
+  padding: .7em 0 .15em;
+  font-size: 1em;
+  font-weight: bold;
+}
+.session--speaker--org {
+  padding: 0;
+  font-size: .8em;
+  color: #00ADD8;
+}
+@media (max-width: 800px) {
+  .session {
+    width: 100%;
+    float: none;
+    margin-bottom: 1em;
   }
-}
-@media (max-width: 400px) {
-  .schedule table {
-    font-size: .5em;
+  .session:first-child {
+    margin-right: 0;
   }
 }
 </style>
