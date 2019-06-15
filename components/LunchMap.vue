551a38fd4ad5b2c9081fd9dad60af9de3b100787
linyows <linyows@gmail.com>
2019-06-15 20:58:09 +0900

    add links

--- components/LunchMap.vue
+++ components/LunchMap.vue
@@ -22,8 +22,8 @@ ja:
       <GmapMap map-type-id="roadmap" :center="center" :zoom="zoom" :options="options" >
         <GmapInfoWindow :options="infoOptions" :position="infoWindowPos" :opened="infoWinOpen" @closeclick="infoWinOpen = false" >
           <div class="gmap--infowin">
-            <p class="gmap--infoimg"> <img :src="infoContent.image" width="200" /> </p>
-            <p class="gmap--infotext"> <a :href="infoContent.url" target="_blank">{{ infoContent.title }}</a> </p>
+            <div class="gmap--infoimg" :style="{ backgroundImage: 'url(' + infoContent.image + ')' }"></div>
+            <p class="gmap--infotext"> <a :href="infoContent.url" target="_blank">{{ infoContent.title }} </a> <i class="gmap--link fas fa-external-link-alt"></i></p>
           </div>
         </GmapInfoWindow>
         <GmapMarker v-for="(m, i) in fgn" @click="toggleInfoWindow(m, i)" :key="i + '-0'" :position="m.position"
@@ -101,14 +101,29 @@ ja:
 
     <div class="lunchmap--footer container">
       <p class="lunch-time">
-        <span class="lunch-time--title">Lunch Time:</span> 11:50 AM - 1:20 PM <span class="lunch-time--duration">(1 hours, 30 min)</span></p>
+        <span class="lunch-time--title">Lunch Time:</span> 11:50 AM - 1:20 PM <span class="lunch-time--duration">(1 hours, 30 min)</span>
+      </p>
+    </div>
+
+    <div class="lunchmap--ps container">
+      <h2>Fukuoka Gourmet Collected</h2>
+      <p>以下、福岡グルメのまとめ記事です。</p>
+      <ul class="colored">
+        <li><a href="https://icotto.jp/presses/3360">【天神ランチ決定版】迷ったらココへ！おすすめ人気店14選</a> by icotto.jp</li>
+        <li><a href="https://tabelog.com/matome/3289/">味・鮮度・コスパも自慢！福岡のおすすめランチ20選</a> by tabelog.com</li>
+        <li><a href="https://kinarino.jp/cat8-%E6%97%85%E8%A1%8C%E3%83%BB%E3%81%8A%E5%87%BA%E3%81%8B%E3%81%91/17580-%E8%A6%B3%E5%85%89%E3%81%AB%E8%A1%8C%E3%81%A3%E3%81%9F%E3%82%89%E3%81%9C%E3%81%B2%E3%80%82%E7%A6%8F%E5%B2%A1%E3%81%A7%E9%A3%9F%E3%81%B9%E3%82%8B%E7%BE%8E%E5%91%B3%E3%81%97%E3%81%84%E3%81%8A%E3%81%99%E3%81%99%E3%82%81%E3%83%A9%E3%83%B3%E3%83%8114%E9%81%B8%E3%80%82">観光に行ったらぜひ。福岡で食べる美味しいおすすめランチ14選。</a> by kinarino.jp</li>
+        <li><a href="https://aumo.jp/articles/18076">【厳選】福岡のご当地グルメ10選！食の都で名物料理を食べつくそう</a> by aumo.jp</li>
+        <li><a href="https://www.jalan.net/news/article/296665/">ご当地グルメ天国！福岡でグルメライターがおすすめしたい名物12選！</a> by jalan.net</li>
+        <li><a href="http://r.gnavi.co.jp/g-interview/entry/gohan/2589">博多～天神周辺のおすすめグルメ！観光しながら名物からB級まで福岡グルメを食べ歩く30選</a> by gnavi.co.jp</li>
+        <li><a href="https://tech.smarthr.jp/entry/2019/03/21/121514">【保存版】自腹でつくる博多グルメマップ #RubyKaigi 2019</a> by smarthr.jp</li>
+      </ul>
     </div>
   </section>
 </template>
 
 <script lang="ts">
 const styles = require('~/static/googlemap-styles/nolabel.json')
-const markers = require('~/static/eat.json')
+const markers = require('~/static/eat/data.json')
 import { Component, Vue } from 'nuxt-property-decorator'
 const fgn = { lat: 33.5888978, lng: 130.394886 }
 const info = { image: '', title: 'Fukuoka Growth Next', url: 'https://growth-next.com/' }
@@ -180,9 +195,19 @@ export default class Location extends Vue {
 .gmap--infowin {
   padding: 1em;
 }
-.gmap--infotext {
+.gmap--infoimg {
+  width: 250px;
+  height: 250px;
+  background-position: center center;
+  background-size: cover;
+  border-radius: 5px;
+}
+.gmap--infowin .gmap--link {
+  margin-left: .5em;
   font-size: 12px;
-  padding-bottom: 0.2em;
+}
+.gmap--infotext {
+  padding: .5em 0;
   font-weight: bold;
 }
 .gmap--infotext a {
